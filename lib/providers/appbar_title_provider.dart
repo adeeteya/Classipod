23b67987b4482c0650b0a192556fc8c666c021c0
Aditya <adeeteya@gmail.com>
2024-11-30 04:58:50 -0500

    🐛 fixed appbar title

--- lib/providers/appbar_title_provider.dart
+++ lib/providers/appbar_title_provider.dart
@@ -1,18 +1,21 @@
 import 'dart:async';
 
 import 'package:classipod/core/extensions.dart';
-import 'package:classipod/providers/display_provider.dart';
+import 'package:classipod/core/routes.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AppBarTitleNotifier extends Notifier<String> {
   AppBarTitleNotifier() : super();
-  Timer _threeSecondTimer = Timer(Duration.zero, () {});
+  // Timer _threeSecondTimer = Timer(Duration.zero, () {});
 
   @override
   String build() {
-    watchScreenChanges();
-    executeEveryMinute();
-    return ref.read(displayProvider).displayScreenType.toString();
+    // executeEveryMinute();
+    return Routes.menu.title;
+  }
+
+  void changeTitle(String title) {
+    state = Routes.values.byName(title).title;
   }
 
   void executeEveryMinute() {
@@ -21,22 +24,10 @@ class AppBarTitleNotifier extends Notifier<String> {
         DateTime(now.year, now.month, now.day, now.hour, now.minute + 1);
     Timer(nextMinute.difference(now), () {
       Timer.periodic(const Duration(minutes: 1), (timer) {
-        if (!_threeSecondTimer.isActive) {
-          state = DateTime.now().getTimeOfTheDayString;
-        }
+        // if (!_threeSecondTimer.isActive) {
+        //   state = DateTime.now().getTimeOfTheDayString;
+        // }
       });
-    });
-  }
-
-  void watchScreenChanges() {
-    final displayDetails = ref.watch(displayProvider);
-    state = displayDetails.displayScreenType.toString();
-    if (_threeSecondTimer.isActive) {
-      _threeSecondTimer.cancel();
-    }
-    _threeSecondTimer = Timer(const Duration(seconds: 3), () {
-      state = DateTime.now().getTimeOfTheDayString;
-      _threeSecondTimer.cancel();
     });
   }
 }

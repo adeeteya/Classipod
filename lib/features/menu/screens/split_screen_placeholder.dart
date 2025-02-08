f224383917012510547b283ae1f6dcb9e0e9a41e
Aditya <adeeteya@gmail.com>
2025-02-08 04:37:03 -0500

    ✨ added on start split screen animation

--- lib/features/menu/screens/split_screen_placeholder.dart
+++ lib/features/menu/screens/split_screen_placeholder.dart
@@ -1,22 +1,66 @@
+import 'dart:async';
+
 import 'package:classipod/core/extensions/build_context_extensions.dart';
 import 'package:classipod/features/menu/controller/split_screen_controller.dart';
 import 'package:classipod/features/menu/models/split_screen_type.dart';
 import 'package:classipod/features/menu/widgets/animated_album_art_scroller.dart';
+import 'package:classipod/features/menu/widgets/icon_preview_widget.dart';
 import 'package:classipod/features/menu/widgets/language_preview_widget.dart';
 import 'package:classipod/features/menu/widgets/now_playing_preview_widget.dart';
 import 'package:classipod/features/menu/widgets/settings_preview_widget.dart';
-import 'package:classipod/features/menu/widgets/icon_preview_widget.dart';
 import 'package:classipod/features/music/songs/songs_provider.dart';
 import 'package:classipod/features/settings/controller/settings_preferences_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SplitScreenPlaceholder extends ConsumerWidget {
+class SplitScreenPlaceholder extends ConsumerStatefulWidget {
   final Widget child;
-  const SplitScreenPlaceholder({super.key, required this.child});
+  const SplitScreenPlaceholder({
+    super.key,
+    required this.child,
+  });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState createState() => _SplitScreenPlaceholderState();
+}
+
+class _SplitScreenPlaceholderState extends ConsumerState<SplitScreenPlaceholder>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+  late final Animation<Offset> _leftSlideAnimation;
+  late final Animation<Offset> _rightSlideAnimation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      reverseDuration: const Duration(milliseconds: 300),
+    );
+    _leftSlideAnimation = Tween<Offset>(
+      begin: const Offset(-1, 0),
+      end: Offset.zero,
+    ).animate(_animationController);
+    _rightSlideAnimation = Tween<Offset>(
+      begin: const Offset(1, 0),
+      end: Offset.zero,
+    ).animate(_animationController);
+    unawaited(_forwardAnimation());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _forwardAnimation() async {
+    await _animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final splitScreenEnabled = ref.watch(
       settingsPreferencesControllerProvider.select((e) => e.splitScreenEnabled),
     );
@@ -101,40 +145,47 @@ class SplitScreenPlaceholder extends ConsumerWidget {
         splitScreenWidget = const AnimatedAlbumArtScroller();
       }
     }
-
     return CupertinoPageScaffold(
       child: splitScreenEnabled
           ? Row(
               children: [
-                Expanded(child: child),
                 Expanded(
-                  child: AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 500),
-                    transitionBuilder: (widget, animation) {
-                      if (splitScreenType == SplitScreenType.albumArt) {
-                        final slideAnimation = Tween<Offset>(
-                          begin: const Offset(1, 0),
-                          end: Offset.zero,
-                        ).animate(animation);
+                  child: SlideTransition(
+                    position: _leftSlideAnimation,
+                    child: widget.child,
+                  ),
+                ),
+                Expanded(
+                  child: SlideTransition(
+                    position: _rightSlideAnimation,
+                    child: AnimatedSwitcher(
+                      duration: const Duration(milliseconds: 500),
+                      transitionBuilder: (widget, animation) {
+                        if (splitScreenType == SplitScreenType.albumArt) {
+                          final slideAnimation = Tween<Offset>(
+                            begin: const Offset(1, 0),
+                            end: Offset.zero,
+                          ).animate(animation);
+                          return FadeTransition(
+                            opacity: animation,
+                            child: SlideTransition(
+                              position: slideAnimation,
+                              child: widget,
+                            ),
+                          );
+                        }
                         return FadeTransition(
                           opacity: animation,
-                          child: SlideTransition(
-                            position: slideAnimation,
-                            child: widget,
-                          ),
+                          child: widget,
                         );
-                      }
-                      return FadeTransition(
-                        opacity: animation,
-                        child: widget,
-                      );
-                    },
-                    child: splitScreenWidget,
+                      },
+                      child: splitScreenWidget,
+                    ),
                   ),
                 ),
               ],
             )
-          : child,
+          : widget.child,
     );
   }
 }

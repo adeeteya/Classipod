fff166e127047895736e3c355e213572b133bca5
Aditya <adeeteya@gmail.com>
2025-05-06 22:49:01 -0400

    ❌ removed unused web file picker impl

--- lib/core/services/audio_files_service.dart
+++ lib/core/services/audio_files_service.dart
@@ -3,16 +3,13 @@ import 'dart:collection';
 import 'dart:io';
 
 import 'package:classipod/core/constants/constants.dart';
-import 'package:classipod/core/constants/keys.dart';
 import 'package:classipod/core/constants/online_audio_files_metadata.dart';
 import 'package:classipod/core/models/music_metadata.dart';
 import 'package:classipod/core/repositories/metadata_reader_repository.dart';
 import 'package:classipod/features/settings/controller/settings_preferences_controller.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
@@ -41,48 +38,7 @@ class AudioFilesServiceNotifier
         );
         // Check if the metadata box is empty
         if (metadataBox.isEmpty) {
-          if (kIsWeb) {
-            // FilePickerWeb.registerWith(Registrar());
-            await showDialog(
-              context: deviceFrameGlobalKey.currentContext!,
-              builder:
-                  (context) => AlertDialog(
-                    title: const Text("Pick Files"),
-                    content: const Text(
-                      "Select all the music files to add to the library.",
-                    ),
-                    actions: [
-                      TextButton(
-                        onPressed: () => context.pop(),
-                        child: const Text("Ok"),
-                      ),
-                    ],
-                  ),
-            );
-            final pickedFiles = await FilePicker.platform.pickFiles(
-              dialogTitle: 'Select Music Files',
-              type: FileType.audio,
-              allowMultiple: true,
-              lockParentWindow: true,
-            );
-            if (pickedFiles != null) {
-              final result = await compute(
-                ref
-                    .read(metadataReaderRepositoryProvider)
-                    .extractMetadataFromFiles,
-                pickedFiles.paths
-                    .where((e) => e != null)
-                    .map((e) => e!)
-                    .toList(growable: false),
-              );
-              await metadataBox.addAll(result);
-              return UnmodifiableListView(result);
-            } else {
-              return UnmodifiableListView([]);
-            }
-          }
-          // Pick Directory on Windows
-          else if (Platform.isWindows) {
+          if (Platform.isWindows) {
             final newDirectory = await FilePicker.platform.getDirectoryPath(
               dialogTitle: "Select Music Directory",
               lockParentWindow: true,

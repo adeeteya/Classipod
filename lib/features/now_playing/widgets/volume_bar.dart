74b94aa1553ecde254e8ada097ac4fb8c428d526
Aditya <adeeteya@gmail.com>
2026-07-24 20:19:53 -0700

    fix: update volume icons to use emphasis color from context

--- lib/features/now_playing/widgets/volume_bar.dart
+++ lib/features/now_playing/widgets/volume_bar.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:classipod/core/constants/app_palette.dart';
+import 'package:classipod/core/extensions/build_context_extensions.dart';
 import 'package:classipod/core/services/audio_player_service.dart';
 import 'package:classipod/features/settings/controller/settings_preferences_controller.dart';
 import 'package:classipod/features/settings/models/volume_mode.dart';
@@ -74,7 +75,11 @@ class _VolumeBarState extends ConsumerState<VolumeBar> {
     return RepaintBoundary(
       child: Row(
         children: [
-          const Icon(CupertinoIcons.volume_down, size: 18),
+          Icon(
+            CupertinoIcons.volume_down,
+            size: 18,
+            color: context.appIconEmphasisColor,
+          ),
           Expanded(
             child: LayoutBuilder(
               builder: (context, constraints) {
@@ -134,7 +139,11 @@ class _VolumeBarState extends ConsumerState<VolumeBar> {
               },
             ),
           ),
-          const Icon(CupertinoIcons.volume_up, size: 18),
+          Icon(
+            CupertinoIcons.volume_up,
+            size: 18,
+            color: context.appIconEmphasisColor,
+          ),
         ],
       ),
     );
